1b8e7c8580f8e779dfec303533ab40ef6b4c0c88
Mohamed Emad <aleslam12345@gmail.com>
2023-07-20 03:42:20 +0300

    add di with dagger hilt

---
+++ app/src/main/java/com/team/cat_hackathon/data/repositories/BaseRepositoryImpl.kt
@@ -0,0 +1,8 @@
+package com.team.cat_hackathon.data.repositories
+
+import android.content.Context
+import com.team.cat_hackathon.data.source.MyDao
+
+class BaseRepositoryImpl (val dao : MyDao , val context : Context) {
+
+}
