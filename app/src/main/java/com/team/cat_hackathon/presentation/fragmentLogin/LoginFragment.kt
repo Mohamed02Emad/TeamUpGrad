65d8ac4b6ef199918d6fec4a2fdfc7ce905b5f57
MaiEl5odery <105084125+MaiElkhodery@users.noreply.github.com>
2023-07-21 03:21:45 +0300

    Modify signup&login

--- app/src/main/java/com/team/cat_hackathon/presentation/fragmentLogin/LoginFragment.kt
+++ app/src/main/java/com/team/cat_hackathon/presentation/fragmentLogin/LoginFragment.kt
@@ -5,19 +5,27 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.widget.AppCompatButton
 import com.team.cat_hackathon.R
+import com.team.cat_hackathon.databinding.FragmentLoginBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [LoginFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class LoginFragment : Fragment() {
+
+
+    private lateinit var binding: FragmentLoginBinding
+    private lateinit var passwordEditView: EditText
+    private lateinit var emailEditView: EditText
+    private lateinit var loginButton: AppCompatButton
+    private lateinit var signupTextView: TextView
+
+
+
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -34,20 +42,13 @@ class LoginFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+
+        binding=FragmentLoginBinding.inflate(layoutInflater)
+        init()
+        return binding.root
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment LoginFragment.
-         */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
             LoginFragment().apply {
@@ -56,5 +57,11 @@ class LoginFragment : Fragment() {
                     putString(ARG_PARAM2, param2)
                 }
             }
+    }
+    private fun init(){
+        emailEditView=binding.editTextEmailLogin
+        passwordEditView=binding.editTextPasswordLogin
+        loginButton=binding.buttonLogin
+        signupTextView=binding.textViewSignup
     }
 }
