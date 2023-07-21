32745a47e8615e4ecb73605d67c95b2597a2942c
Mohamed Emad <aleslam12345@gmail.com>
2023-07-21 12:48:09 +0300

    fix gradle error

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,9 +53,6 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("androidx.core:core-ktx:1.10.1")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
-    testImplementation("junit:junit:4.12")
-    androidTestImplementation("androidx.test.ext:junit:1.1.1")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.2.0")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.work:work-runtime-ktx:2.8.1")
@@ -98,7 +95,6 @@ dependencies {
     // dagger hilt
     implementation("com.google.dagger:hilt-android:2.44")
     kapt("com.google.dagger:hilt-android-compiler:2.44")
-
 
     //dataStore
     implementation("androidx.datastore:datastore-preferences:1.0.0")
