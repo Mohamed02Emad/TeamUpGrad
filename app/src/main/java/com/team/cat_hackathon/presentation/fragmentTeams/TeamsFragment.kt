c54e8520e9a25acc8a6f7faccf8c0f492b4a323d
MaiEl5odery <105084125+MaiElkhodery@users.noreply.github.com>
2023-07-22 22:01:00 +0300

    modify fragment_teams ui and logic

--- app/src/main/java/com/team/cat_hackathon/presentation/fragmentTeams/TeamsFragment.kt
+++ app/src/main/java/com/team/cat_hackathon/presentation/fragmentTeams/TeamsFragment.kt
@@ -5,56 +5,67 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isGone
+import androidx.recyclerview.widget.GridLayoutManager
 import com.team.cat_hackathon.R
+import com.team.cat_hackathon.data.models.User
+import com.team.cat_hackathon.databinding.FragmentHomeBinding
+import com.team.cat_hackathon.databinding.FragmentTeamsBinding
+import com.team.cat_hackathon.presentation.adapters.TeamAdapter
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [TeamsFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class TeamsFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    private lateinit var binding: FragmentTeamsBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
         }
     }
 
     override fun onCreateView(
+
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_teams, container, false)
+        binding = FragmentTeamsBinding.inflate(layoutInflater)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initRecyclerView()
+
+    }
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment TeamsFragment.
-         */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
-        fun newInstance(param1: String, param2: String) =
+        fun newInstance() =
             TeamsFragment().apply {
                 arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
                 }
             }
+    }
+
+    private fun initRecyclerView(){
+        val recyclerView =binding.recyclerViewInTeam
+        recyclerView.layoutManager = GridLayoutManager(requireContext(),2)
+        recyclerView.adapter= TeamAdapter()
+    }
+    private fun haveNoTeamUiVisibility(value : Boolean){
+        binding.groupNotInTeam.isGone = !value
+    }
+    private fun haveTeamUiVisibility(value : Boolean){
+        binding.groupInTeam.isGone = !value
+    }
+    private fun onClickBack(){
+        binding.backButton.setOnClickListener{
+
+        }
+    }
+    private fun onClickJoin(){
+        binding.joinTextInTeam.setOnClickListener{
+
+        }
     }
 }
