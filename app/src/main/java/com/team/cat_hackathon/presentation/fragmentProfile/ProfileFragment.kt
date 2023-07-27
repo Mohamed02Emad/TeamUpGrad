f4fd80c5d4343ddfcf752e8b1400e0e0f8c1a589
Mohamed Emad <aleslam12345@gmail.com>
2023-07-27 18:36:37 +0300

    add fragment profile ui

---
+++ app/src/main/java/com/team/cat_hackathon/presentation/fragmentProfile/ProfileFragment.kt
@@ -0,0 +1,23 @@
+package com.team.cat_hackathon.presentation.fragmentProfile
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.team.cat_hackathon.R
+import com.team.cat_hackathon.databinding.FragmentProfileBinding
+
+
+class ProfileFragment : Fragment() {
+
+    private lateinit var binding : FragmentProfileBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentProfileBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+}
